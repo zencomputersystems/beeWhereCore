6e996fa3df62116f558a2076310dc8a284696a43
Fakhri <fakhrialuwe@rocketmail.com>
2020-10-01 16:28:55 +0800

    validate user-info api if calendar has no restday

--- src/api/user-info/user-info.service.ts
+++ src/api/user-info/user-info.service.ts
@@ -2,6 +2,7 @@ import { UserprofileDbService, WorkingHourDbService, CalendarProfileDbService, C
 import { map, mergeMap } from 'rxjs/operators';
 import { Injectable } from '@nestjs/common';
 import { ResultUserInfoDTO, WorkingHourSettingDTO, ProfileSettingDetailDTO, CalendarSettingDTO } from './dto/result-user-info.dto';
+import { of } from 'rxjs';
 var { convertXMLToJson } = require('@zencloudservices/xmlparser');
 @Injectable()
 export class UserInfoService {
@@ -17,23 +18,48 @@ export class UserInfoService {
 
     return this.userprofileDbService.findByFilterV4([[], [`(USER_GUID=${userId})`], null, null, null, [], null]).pipe(
       mergeMap(res => {
+        // setup global profile data 
         profileData = res[0];
-        return this.workingHourDbService.findByFilterV4([[], [`(WORKING_HOURS_GUID=${res[0].WORKING_HOURS_GUID})`], null, null, null, [], null]);
+        // find working hour data
+        if (res[0].WORKING_HOURS_GUID != null) {
+          return this.workingHourDbService.findByFilterV4([[], [`(WORKING_HOURS_GUID=${res[0].WORKING_HOURS_GUID})`], null, null, null, [], null]).pipe(
+            map(res2 => {
+              let workingHourData = convertXMLToJson(res2[0].PROPERTIES_XML);
+              workingHourTime = workingHourData.property.fullday;
+
+              // send empty data if have working hour id but not assign fullday time
+              if (workingHourTime == undefined) {
+                workingHourTime = [];
+              }
+
+              return res2;
+            })
+          );
+        } else {
+          // send null working hour data if not assign
+          workingHourTime = null;
+          return of(res[0]);
+        }
       }), mergeMap(res => {
-
-        return this.calendarProfileDetailDbService.findByFilterV4([[], [`(CALENDAR_GUID=${profileData.CALENDAR_GUID})`, `AND (YEAR=${new Date().getFullYear()})`], null, null, null, [], null]).pipe(
-          map(res2 => {
-            let workingHourData = convertXMLToJson(res[0].PROPERTIES_XML);
-            let calendarData = convertXMLToJson(res2[0].PROPERTIES_XML);
-
-            workingHourTime = workingHourData.property.fullday;
-            calendarRestDay = calendarData.rest;
-
-            return res2;
-          })
-        )
+        // find calendar data to find rest day
+        if (profileData.CALENDAR_GUID != null) {
+          return this.calendarProfileDetailDbService.findByFilterV4([[], [`(CALENDAR_GUID=${profileData.CALENDAR_GUID})`, `AND (YEAR=${new Date().getFullYear()})`], null, null, null, [], null]).pipe(
+            map(res2 => {
+              let calendarData = convertXMLToJson(res2[0].PROPERTIES_XML);
+              calendarRestDay = calendarData.rest;
+              // send empty array if have calendar id but not assign rest day
+              if (calendarRestDay == undefined) {
+                calendarRestDay = [];
+              }
+              return res2;
+            })
+          )
+        } else {
+          // send null calendar rest day data if not assign
+          calendarRestDay = null;
+          return of(res[0]);
+        }
       }), map(res => {
-
         let responseData = new ResultUserInfoDTO();
         responseData.userId = profileData.USER_GUID;
         responseData.email = profileData.EMAIL;
@@ -41,16 +67,26 @@ export class UserInfoService {
         responseData.profilePictureUrl = process.env.URL_STORAGE_ELEAVE + profileData.PROFILE_PICTURE;
 
         let workingHourTemp = new WorkingHourSettingDTO;
-        workingHourTemp.start = workingHourTime['start_time'];
-        workingHourTemp.end = workingHourTime['end_time'];
 
-        let restDay = [];
-        calendarRestDay.forEach(element => {
-          restDay.push(element.fullname.toLowerCase());
-        });
+        if (workingHourTime == null) {
+          workingHourTemp = null;
+        }
+        else {
+          workingHourTemp.start = workingHourTime['start_time'];
+          workingHourTemp.end = workingHourTime['end_time'];
+        }
 
         let calendarTemp = new CalendarSettingDTO;
-        calendarTemp.restday = restDay;
+
+        if (calendarRestDay == null) {
+          calendarTemp = null;
+        } else {
+          let restDay = [];
+          calendarRestDay.forEach(element => {
+            restDay.push(element.fullname.toLowerCase());
+          });
+          calendarTemp.restday = restDay;
+        }
 
         let profileSetting = new ProfileSettingDetailDTO;
         profileSetting.calendar = calendarTemp;
